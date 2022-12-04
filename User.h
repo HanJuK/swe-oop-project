6d5d09a72826bce5bb7d41f5acbec1bd2d53e031
HanJuK <qwerty03081@gmail.com>
2022-12-04 22:15:41 +0900

    User class (basic)

---
+++ User.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class User
+{
+	std::string id;
+	std::string pw;
+	std::string name;
+	std::string phone;
+	int seatNo;
+	bool isBlocked;
+
+public:
+	User(std::string id, std::string pw, std::string name, std::string phone)
+	{
+		this->id = id;
+		this->pw = pw;
+		this->name = name;
+		this->phone = phone;
+		this->seatNo = -1;
+		this->isBlocked = false;
+	}
+
+	void updateUserInfo(std::string id, std::string pw, std::string name, std::string phone)
+	{
+		this->id = id;
+		this->pw = pw;
+		this->name = name;
+		this->phone = phone;
+
+		return;
+	}
+
+	void reserveSeat(int seatNo)
+	{
+		if (seatNo == -1)
+		{
+			this->seatNo = seatNo;
+
+			// TODO: logic
+		}
+		else
+		{
+			std::cout << "Seat already reserved for this user!\n";
+		}
+
+		return;
+	}
+
+	void extendSeat()
+	{
+		if (seatNo == -1)
+		{
+			std::cout << "No seat reserved for this user!\n";
+		}
+		else
+		{
+			// TODO: logic
+		}
+
+		return;
+	}
+
+	void cancelSeat()
+	{
+		if (seatNo == -1)
+		{
+			std::cout << "No seat reserved for this user!\n";
+		}
+		else
+		{
+			this->seatNo = -1;
+
+			// TODO: logic
+		}
+
+		return;
+	}
+
+	~User()
+	{
+		std::cout << "User " << this->name << " (" << this->phone << ") deleted!\n";
+	}
+};
