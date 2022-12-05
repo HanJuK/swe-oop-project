ac55244d9e09ad01e288e66712795b7275543eb7
HanJu Kim <48195650+HanJuK@users.noreply.github.com>
2022-12-05 14:09:44 +0900

    Admin class (basic)

---
+++ Admin.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Admin
+{
+	std::string id;
+	std::string pw;
+	std::string name;
+	std::string phone;
+
+public:
+	Admin(std::string id, std::string pw, std::string name, std::string phone)
+	{
+		this->id = id;
+		this->pw = pw;
+		this->name = name;
+		this->phone = phone;
+	}
+
+	void updateAdminInfo(std::string id, std::string pw, std::string name, std::string phone)
+	{
+		this->id = id;
+		this->pw = pw;
+		this->name = name;
+		this->phone = phone;
+
+		return;
+	}
+
+	void viewAllUserInfo()
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	void blockUser(std::string phone)
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	void addSeat(int seatNo)
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	void removeSeat(int seatNo)
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	void makeSeatTemporaryUnavailable(int seatNo)
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	void changeDefaultReservationTime(int minutes)
+	{
+		// TODO: logic
+
+		return;
+	}
+
+	~Admin()
+	{
+		std::cout << "Admin " << this->name << " (" << this->phone << ") deleted!\n";
+	}
+};
